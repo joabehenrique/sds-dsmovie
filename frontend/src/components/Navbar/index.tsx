6201f5639c1332b49e3db4c705efaab00dee0893
joabehenrique <henriquecidoz@hotmail.com>
2022-01-12 18:30:59 -0300

    fix: ajustado tag anchor para ter sucesso na compilaçao do projeto no netlify

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,11 +7,12 @@ function Navbar() {
             <nav className='container'>
                 <div className='dsmovie-nav-content'>
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/joabehenrique"></a>
-                    <div className='dsmovie-contact-container'>
-                        <GithubIcon />
-                        <p className='dsmovie-contact-link'>/joabehenrique</p>
-                    </div>
+                    <a href="https://github.com/joabehenrique" className="link-light">
+                        <div className='dsmovie-contact-container'>
+                            <GithubIcon />
+                            <p className='dsmovie-contact-link'>/joabehenrique</p>
+                        </div>
+                    </a>
                 </div>
             </nav>
         </header>
