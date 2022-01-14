ac7e6c9b168a4d1cb27e77591544c073d62d7846
joabehenrique <henriquecidoz@hotmail.com>
2022-01-14 14:58:19 -0300

    styles: alterado style da tag anchor

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className='container'>
                 <div className='dsmovie-nav-content'>
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/joabehenrique" className="link-light">
+                    <a href="https://github.com/joabehenrique" className="link-unstyled">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
                             <p className='dsmovie-contact-link'>/joabehenrique</p>
