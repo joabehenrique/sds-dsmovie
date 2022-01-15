538fe03d7f6b934cb49947b68dad97647e3f4379
joabehenrique <henriquecidoz@hotmail.com>
2022-01-15 09:54:36 -0300

    style: alterado alguns styles do component navbar e formatado o text do title page

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
         <header>
             <nav className='container'>
                 <div className='dsmovie-nav-content'>
-                    <h1>DSMovie</h1>
+                    <h1>JH DSMovie</h1>
                     <a href="https://github.com/joabehenrique" className="link-unstyled">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
